bd68b571e3afe22c9b077cce7b919b66185c0e33
Harsh Mohan <harshmohan813@gmail.com>
2021-12-18 01:45:32 +0000

    Added Relative Path For NodeJs
    Fixed The Bug: itemName.json not found.

--- tf2Items.js
+++ tf2Items.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+const fs = require("fs");
+var path = require("path");
 
-let rawdataItems = fs.readFileSync('./Data/itemName.json');
+let rawdataItems = fs.readFileSync(path.join(__dirname, "data", "itemName.json"));
 let dataItems = JSON.parse(rawdataItems);
 
 exports.getItemName = function(itemID) {
